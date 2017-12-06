7eec1e0d7e8ca9948867932ea6dbaaceab0c6065
matrumz <matrumz@hotmail.com>
2017-12-05 22:28:30 -0500

    Configuration binary updated with directory creation

--- scripts/setup.js
+++ scripts/setup.js
@@ -1,6 +1,24 @@
 #!/usr/bin/env node
 
 const ServerConfig = require("../dist/config");
+const fs = require("fs");
 
-const config = new ServerConfig.Config();
-config.dump();
+/* Gen or load config file */
+var config;
+try {
+    config = new ServerConfig.Config();
+    config.dump();
+}
+catch (e) {
+    throw new Error("An error occurred while initializing the configuration file: " + e.toString());
+}
+
+/* Create controllers directory if needed */
+try {
+    if (!fs.existsSync(config.routes.controllerRootDir)) {
+        fs.mkdirSync(config.routes.controllerRootDir);
+    }
+}
+catch (e) {
+    throw new Error("An error occurred while initializing the controllers directory: " + e.toString());
+}
