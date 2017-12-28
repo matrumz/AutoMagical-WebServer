3d28f031c80d053e705197b7e7286f3dd2597fef
matrumz <matrumz@hotmail.com>
2017-12-27 20:18:58 -0500

    test.bash slightly more verbose
    trying to diagnose why the response in travis fails while it works on my machine

--- test/test.bash
+++ test/test.bash
@@ -6,6 +6,7 @@ amwebs &
 sleep 5
 RES=$(curl http://localhost:3000/test_1_controller)
 killall node
+echo "Test controller root received: $RES"
 if [ "$RES" != "root pass" ]; then
     echo "Test controller root FAILED"
     exit 1;
