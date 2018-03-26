3039d6db5216eeafcf917c6c22ad5b2348741490
matrumz <matrumz@hotmail.com>
2018-03-26 19:18:27 -0400

    Delete all the old crap

--- scripts/startGetControllersDir.js
+++
@@ -1,8 +0,0 @@
-const os = require("os");
-const path = require("path");
-const ServerConfig = require("../dist/config");
-
-const config = new ServerConfig.Config();
-config.dump();
-
-console.log(path.resolve(os.homedir(config.routes.controllerRootDir), config.routes.controllerRootDir));
