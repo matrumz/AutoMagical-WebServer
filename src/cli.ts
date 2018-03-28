963dbac82d4ae15925b39b0c23d4007d771ba398
matrumz <matrumz@hotmail.com>
2018-03-27 20:59:03 -0400

    Help documentation started

--- src/cli.ts
+++ src/cli.ts
@@ -16,29 +16,67 @@ class CLI
          * Parse CLI,
          * Get the amwebs command and arguments
          */
-        const { command, argv } = clCommands(constants.cli.commands);
-        const args: any = clArgs(constants.cli.options, { partial: true, argv: argv })[command];
+        var command: string;
+        var argv: string[];
+        var args: any;
+        var commandArgs: any;
+        /* Get command */
+        try {
+            let parsedCmd: { command: string, argv: string[] };
+            parsedCmd = clCommands(constants.cli.commands);
+            argv = parsedCmd.argv;
+            command = parsedCmd.command;
+        }
+        catch (e) {
+            console.log(constants.cli.usage.commands);
+            return;
+        }
+        /* Get args */
+        try {
+            args = clArgs(constants.cli.options, { argv: argv, partial: true, camelCase: true } as clArgs.Options);
+            commandArgs = args[command];
+        }
+        catch (e) {
+            console.log(constants.cli.usage.options[command]);
+        }
+
+        /* Help messages */
+        if (args._all.help) {
+            if (command) {
+                console.log(constants.cli.usage.options[command] || "There is no help for this command.");
+            }
+            else {
+                console.log(constants.cli.usage.commands)
+            }
+            return;
+        }
 
         switch (command) {
 
             case "generate":
                 CLI.generate({
-                    controllers: args.controller,
-                    sample: args.sample
+                    controllers: commandArgs.controller,
+                    sample: commandArgs.sample
                 });
                 break;
 
             case "start":
                 CLI.start({
-                    port: args.port,
-                    controllers: args.controllers,
-                    controllerPattern: args.controllerPattern,
-                    log: args.log
+                    port: commandArgs.port,
+                    controllers: commandArgs.controllers,
+                    controllerPattern: commandArgs.controllerPattern,
+                    log: commandArgs.log
                 });
                 break;
 
             case "test":
                 CLI.test();
+                break;
+
+            default:
+                console.error("Woah there. I see you found an un-instantiated command. Please send me an email at the below address and include the command you used (" + (command || "NULL") + ") and the version of AutoMagical-WebServer that you are using.");
+                console.error(constants.support.email);
+                process.exit(constants.exitCodes.uninstantiatedCommand)
         }
     }
 
--- src/lib/data/constants.ts
+++ src/lib/data/constants.ts
@@ -1,4 +1,5 @@
 import * as clArgs from "command-line-args";
+const clUsage = require("command-line-usage");
 
 export namespace cli
 {
@@ -9,6 +10,12 @@ export namespace cli
     ];
 
     export const options: clArgs.OptionDefinition[] = [
+        {
+            name: "help",
+            alias: "h",
+            type: Boolean,
+            multiple: false
+        },
         {
             name: "controller",
             alias: "c",
@@ -22,7 +29,6 @@ export namespace cli
             alias: "s",
             type: String,
             multiple: false,
-            defaultOption: false,
             group: ["generate"]
         },
         {
@@ -40,7 +46,7 @@ export namespace cli
             group: ["start", "generate"]
         },
         {
-            name: "controllerPattern",
+            name: "controller-pattern",
             alias: "R",
             type: String,
             multiple: false,
@@ -53,5 +59,52 @@ export namespace cli
             multiple: false,
             group: ["start"]
         }
-    ]
+    ];
+
+    export namespace usage
+    {
+        export const commands: any = clUsage([
+            {
+                header: "AutoMagical-WebServer (amwebs)",
+                content: 'A skeleton Node/Express web server that automagically accepts web-requests on URIs dynamically generated from "plug-and-play" controller modules. A great starting point for RESTful newbs and casual DIY-ers. Inspired by Dan Wahlin\'s Pluralsight course: Integrating Angular with Node.js RESTful Services.'
+            },
+            {
+                header: "Synopsis",
+                content: "amwebs <command> <options>"
+            },
+            {
+                header: "Command List",
+                content: [
+                    { name: "start", summary: "Starts the webserver." },
+                    { name: "generate", summary: "Generate sample and skeleton controllers." },
+                    { name: "test", summary: "Verify your installed version of amwebs." }
+                ]
+            }
+        ]);
+
+        export const options: any = {
+            start: clUsage([
+                {
+                    header: "amwebs - start",
+                    content: "Starts the webserver."
+                },
+                {
+                    header: "Synopsis",
+                    content: [
+                        "$ amwebs start [{bold --port} #] [{bold --controller-pattern} regex] [{bold --log} file] [{bold controllers} file-list]"
+                    ]
+                }
+            ])
+        }
+    }
+}
+
+export namespace support
+{
+    export const email = "admin@nobugs-justfeatures.com";
+}
+
+export namespace exitCodes
+{
+    export const uninstantiatedCommand = 1;
 }
